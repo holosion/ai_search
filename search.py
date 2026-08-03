643377a37ea01da0955b0db82665e8c3ded6f03d
Akena Jonathana <akenajonathan24@gmail.com>
2026-08-03 20:28:03 +0300

    adding the neighbours  method

--- search.py
+++ search.py
@@ -52,6 +52,16 @@ class StackFrontier:
             return node
         
 
+def neighbors(state):
+    row, col = state
+
+    candidates=[
+        ("up", (row -1, col)),
+        ("down", (row +1, col)),
+        ("left", (row, col -1)), 
+        ("right",(row, col +1)),
+    ]
+
 
 
 
