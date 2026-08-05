b7e54f6078109b781edb74bbc73d5f90b359ec76
Akena Jonathana <akenajonathan24@gmail.com>
2026-08-05 23:02:03 +0300

    remove redundant neighbors function from Maze class

--- search.py
+++ search.py
@@ -35,15 +35,7 @@ class StackFrontier:
             return node
         
 
-#def neighbors(state):
-    row, col = state
 
-    candidates=[
-        ("up", (row -1, col)),
-        ("down", (row +1, col)),
-        ("left", (row, col -1)), 
-        ("right",(row, col +1)),
-    ]
 
 
 class Maze:
